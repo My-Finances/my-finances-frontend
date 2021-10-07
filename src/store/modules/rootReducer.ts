b44b351b1de06dd0d5101da147a268961e50b3cd
Gabriel-Monteiro7 <gabrielmont713@gmail.com>
2021-10-07 09:27:13 -0300

    feat: Adding user redux

---
+++ src/store/modules/rootReducer.ts
@@ -0,0 +1,12 @@
+import { combineReducers } from "redux";
+import auth from "./auth/reducer";
+import user from "./user/reducer";
+import root from "./root/reducer";
+
+const reducers = combineReducers({
+  auth,
+  user,
+  root,
+});
+
+export default reducers;
